056341134bbf101630f66fa5f732e1500d646422
jagLas <jglasher@gmail.com>
2022-12-05 15:57:01 -0500

    completed practice

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,25 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+  static compareGPA(student1, student2) {
+    let betterStudent;
+    if(student1.GPA === student2.GPA) {
+      return "Both students have the same GPA"
+    } else if (student1.GPA > student2.GPA) {
+      betterStudent = student1;
+    } else {
+      betterStudent = student2;
+    }
+    return `${betterStudent.firstName} ${betterStudent.lastName} has the higher GPA.`
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,18 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor (firstName, lastName, subject, yearsOfExperience) {
+    super (firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.yearsOfExperience, 0
+    )
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
